3b63a52a4a89b8a6db195d632395244d7bde2067
David Andersen <davidwoodsandersen@gmail.com>
2018-12-02 16:01:36 -0500

    adds basic implementation

--- src/index.js
+++ src/index.js
@@ -15,7 +15,45 @@ class OutputTransformPlugin {
 		this.options = options;
 	}
 
-	apply(compiler) {}
+	/**
+	 * Returns a map of output asset
+	 * filenames to their transform action.
+	 * @param {object} assets
+	 * @returns {object}
+	 */
+	getTargetMap(assets) {
+		var map = {};
+
+		if (typeof this.options.all === 'function') {
+			Object.keys(assets).forEach((asset) => {
+				map[asset] = this.options.all;
+			});
+		} else if (Array.isArray(this.options.entries)) {
+			this.options.entries.forEach((entry) => {
+				map[entry] = this.entries[entry];
+			});
+		} else {
+			throw new Error('Must include an "all" field or an "entries" field.');
+		}
+
+		return map;
+	}
+
+	/**
+	 * Called by webpack.
+	 * @param {object} compiler
+	 */
+	apply(compiler) {
+		compiler.hooks.emit.tap('OutputTransformPlugin', params => {
+			var map = this.getTargetMap(params.assets);
+
+			for (var entry in map) {
+				if (map.hasOwnProperty(entry)) {
+					params.assets[entry]._value = map[entry](params.assets[entry]._value);
+				}
+			}
+		});
+	}
 }
 
-export default OutputTransformPlugin;
+module.exports = OutputTransformPlugin;
